318d403dc64c6b5957cee965026d2815b4ddb481
Letian Xie <43816414+KasterMist@users.noreply.github.com>
2025-08-15 17:50:12 +0800

    modify snacks config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -229,25 +229,22 @@ return {
             :map("<leader>tS")
 
           -- Create some toggle mappings
-          Snacks.toggle.dim():map("<leader>tD")
-
-          Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>ts")
-          Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>tw")
-          Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>tL")
-          Snacks.toggle.diagnostics():map("<leader>td")
-          Snacks.toggle.line_number():map("<leader>tl")
-          Snacks.toggle
-            .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
-            :map("<leader>tc")
-          Snacks.toggle.treesitter():map("<leader>tT")
-          Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>tb")
-          Snacks.toggle.inlay_hints():map("<leader>th")
-          Snacks.toggle.indent():map("<leader>tg")
-          Snacks.toggle.dim():map("<leader>tD")
+          Snacks.toggle.dim():map("<leader>tD") -- Toggle dimming of inactive windows 
+          Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>ts") -- Toggle spell checking
+          Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>tw") -- Toggle line wrapping
+          Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>tL") -- Toggle relative line numbers
+          Snacks.toggle.diagnostics():map("<leader>td") -- Toggle diagnostics (like LSP errors/warnings)
+          Snacks.toggle.line_number():map("<leader>tl") -- Toggle absolute line numbers
+          Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>tc") -- Toggle conceal level for hidden text
+          Snacks.toggle.treesitter():map("<leader>tT") -- Toggle Treesitter highlighting
+          Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>tb") -- Toggle background between light and dark
+          Snacks.toggle.inlay_hints():map("<leader>tih") -- Toggle inlay hints (LSP hints in code)
+          Snacks.toggle.indent():map("<leader>tg") -- Toggle indentation guides
+          Snacks.toggle.dim():map("<leader>tD") -- Toggle dimming of inactive windows (duplicate)
           -- Toggle the profiler
-          Snacks.toggle.profiler():map("<leader>tpp")
+          Snacks.toggle.profiler():map("<leader>tpp") -- Toggle performance profiler
           -- Toggle the profiler highlights
-          Snacks.toggle.profiler_highlights():map("<leader>tph")
+          Snacks.toggle.profiler_highlights():map("<leader>tph") -- Toggle highlights for profiler results
 
           vim.keymap.del("n", "grn")
           vim.keymap.del("n", "gra")
